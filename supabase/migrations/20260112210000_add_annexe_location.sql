91364958d80994f5b8d544d771786e67686cd145
Topher Brown <206988+topher200@users.noreply.github.com>
2026-01-16 15:26:01 -0500

    feat: add Annexe location (#11)

---
+++ supabase/migrations/20260112210000_add_annexe_location.sql
@@ -0,0 +1,9 @@
+-- Add Annexe location
+INSERT INTO locations (name, address, latitude, longitude, description)
+VALUES (
+  'Annexe',
+  '105 S Mangum St Unit #1, Durham, NC 27701, USA',
+  35.9947192,
+  -78.90079329999999,
+  'Located in Bar Virgile. Wine bar with thoughtful small plates. Price range: $20-30 per person.'
+);
